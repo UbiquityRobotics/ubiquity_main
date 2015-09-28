c62f00b21c462a059a27d03499992e070eba0003
Wayne C. Gramlich <Wayne@Gramlich.Net>
2015-09-28 06:24:53 -0700

    Updated configure.py.

--- configure.py
+++ configure.py
@@ -30,9 +30,10 @@ def main():
 	print("File '{0}' does not exist!".format(hostname_file_name))
 	return 1
     hostname_file = open(hostname_file_name, "ra")
-    hostname = hostname_file.read().strip()
+    old_hostname = hostname_file.read().strip()
     hostname_file.close()
-    print("hostname='{0}'".format(hostname))
+    new_hostname = old_hostname
+    print("hostname='{0}'".format(old_hostname))
 
     # Read hosts file:
     hosts_file_name = "{0}/etc/hosts".format(root)
@@ -42,9 +43,10 @@ def main():
     hosts_file = open(hosts_file_name, "ra")
     hosts_lines = hosts_file.readlines()
     hosts_file.close()
-
-    for hosts_line in hosts_lines:
-	print("{0}".format(hosts_line.strip()))
+    for index in range(len(hosts_lines)):
+	hosts_line = hosts_lines[index].strip()
+	hosts_lines[index] = hosts_line
+	print("'{0}'".format(hosts_line))
 
     # Read in wpa_supplicant if it exists:
     conf_file_name = \
@@ -96,7 +98,7 @@ def main():
 	    # List possibilities and prompt for input command:
 	    print("")
 	    print("[0]: Exit without save")
-	    print("[1]: Manage hostname (i.e. robot name)")
+	    print("[1]: Change current hostname ('{0}')".format(new_hostname))
 	    print("[2]: Manage hostname WiFi access points")
 	    print("[3]: Save everything and exit")
 	    try:
@@ -110,7 +112,7 @@ def main():
 		break
 	    elif command == 1:
 		# Manage the hostname:
-	        print("hostname code not ready yet")
+	        new_hostname = raw_input("New Hostname: ").strip()
 	    elif command == 2:
 		# Manage Wifi access points:
 		while True:
@@ -244,13 +246,57 @@ def main():
 
 		# Write out the `/etc/hostname` file:
 		hostname_file = open(hostname_file_name, "wa")
-		hostname_file.write("{0}\n".format(hostname))
+		hostname_file.write("{0}\n".format(new_hostname))
 		hostname_file.close()
+
+		# Sweep through *hosts_lines*:
+		have_localhost = False
+		have_hostname = False
+		have_zero_conf = False
+		inesrt_index = 0
+		for index in range(len(hosts_lines)):
+		    hosts_line = hosts_lines[index]
+		    if hosts_line.startswith("127.0.0.1"):
+			# We have a loopback address:
+			print("old_host_line='{0}'".format(hosts_line))
+			old_names = hosts_line.split()[1:]
+			new_names = []
+			for old_name in old_names:
+			    new_name = old_name
+			    if old_name.endswith("localhost"):
+				have_localhost = True
+				insert_index = index + 1
+			    elif old_name.endswith(".local"):
+				new_name = "{0}.local".format(new_hostname)
+				have_zero_conf = True
+				insert_index = index + 1
+                            elif old_name == old_hostname:
+				new_name = new_hostname
+				have_hostname = True
+				insert_index = index + 1
+			    else:
+				pass
+			    new_names.append(new_name)
+			host_line = ' '.join(["127.0.0.1"] + new_names)
+			hosts_lines[index] = host_line
+			print("new_host_line='{0}'".format(host_line))
+
+		# Insert any missing bindings to `127.0.0.1`:
+		if not have_localhost:
+		    hosts_lines.insert(insert_index, "127.0.0.1 localhost")
+                    insert_index += 1
+		if not have_hostname:
+		    hosts_lines.insert(insert_index,
+		      "127.0.0.1 {0}".format(new_hostname))
+		if not have_zero_conf:
+		    hosts_lines.insert(insert_index,
+		      "127.0.0.1 {0}.local".format(new_hostname))
 
 		# Write out the `/etc/hosts` file:
 		hosts_file = open(hosts_file_name, "wa")
 		for hosts_line in hosts_lines:
 		    hosts_file.write(hosts_line)
+		    hosts_file.write('\n')
 		hosts_file.close()
 
 		break
