e5d0e4e494402cd8e0806cbe81bcb616fc37d86f
Rohan Agrawal <send2arohan@gmail.com>
2016-02-09 17:07:23 -0800

    added network manager test code

---
+++ configure/test_nm.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2016 by Rohan Agrawal.  All rights reserved.
+
+# This program is used to configure a robot system.
+#
+# Currently, this program permits the following:
+#
+# * Changing the host name.
+#
+# * Configuring WiFi
+
+import os
+import os.path
+import sys
+
+import NetworkManager
+
+for x in NetworkManager.Settings.ListConnections():
+	if x.GetSettings()['connection']['type'] == '802-11-wireless':
+		print x.GetSettings()['802-11-wireless']['ssid']
+
+print ""
+print ""
+
+# print NetworkManager.NetworkManager.GetDevices()[1].DeviceType
+# for x in NetworkManager.NetworkManager.GetDevices()[1].GetAccessPoints():
+# 	print x.ssid
+
+for device in NetworkManager.NetworkManager.GetDevices():
+	if device.DeviceType != NetworkManager.NM_DEVICE_TYPE_WIFI:
+		continue
+	print("Visible on %s" % device.Udi[device.Udi.rfind('/')+1:])
+	device = device.SpecificDevice()
+	active = device.ActiveAccessPoint
+	aps = device.GetAccessPoints()
+	for ap in aps:
+		prefix = '* ' if ap.object_path == active.object_path else '  '
+		print("%s %s" % (prefix, ap.Ssid))
+# print x.Interface
