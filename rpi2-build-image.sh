e55fdf62f987b1d14f7ff37637ad136666b73a80
Rohan Agrawal <send2arohan@gmail.com>
2016-04-22 20:15:24 -0700

    add gui back

--- rpi2-build-image.sh
+++ rpi2-build-image.sh
@@ -205,8 +205,8 @@ chroot $R adduser --gecos "Ubuntu user" --add_extra_groups --disabled-password u
 chroot $R usermod -a -G sudo,adm,netdev -p '$6$iTPEdlv4$HSmYhiw2FmvQfueq32X30NqsYKpGDoTAUV2mzmHEgP/1B7rV3vfsjZKnAWn6M2d.V2UsPuZ2nWHg1iqzIu/nF/' ubuntu
 
 # Install GUI
-#chroot $R apt-get -y install lubuntu-desktop
-#chroot $R apt-get -y install xterm
+chroot $R apt-get -y install lubuntu-desktop
+chroot $R apt-get -y install xterm
 
 # Restore standard sources.list if a local mirror was used:
 if [ -n "$LOCAL_MIRROR" ]; then
