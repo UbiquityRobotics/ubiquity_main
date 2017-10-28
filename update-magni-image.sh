bfcfabd4d4adf66c52515a2d6aae3a28aba785dd
Jim Vaughan <jimv@mrjim.com>
2017-10-28 13:10:52 -0700

    Add Magni image update script (#78)
    * First version of update script

---
+++ update-magni-image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+:<<"END_COMMENT"
+
+ This script updates a system updated from the ubiquity image to incoporate changes
+ made since the last release
+
+ Invoke with this command:
+
+ wget -O - https://raw.githubusercontent.com/UbiquityRobotics/ubiquity_main/kinetic/update-magni-image.sh | bash
+
+END_COMMENT
+
+function get_source {
+  pkg=$1
+  echo "Installing ${pkg} from source"
+  if [ -d ~/catkin_ws/src/${pkg} ]; then
+     echo "Source directory exists"
+     cd ~/catkin_ws/src/${pkg} && git pull
+  else
+    cd ~/catkin_ws/src && git clone https://github.com/UbiquityRobotics/${pkg}.git
+  fi
+}
+
+# We are always going to want to do this
+echo "Upgrading installed packages"
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Install some packages if not already installed
+echo "Installing extra packages"
+sudo apt-get install ros-kinetic-teleop-twist-keyboard
+
+# If current packages are not available via debs, then build from source
+if [[ ! `apt-cache policy ros-kinetic-move-basic` =~ "Installed: 0.2.2" ]]; then
+   get_source 'move_basic'
+fi
+
+get_source 'ubiquity_launches'
+
+echo "Bullding"
+cd ~/catkin_ws && catkin_make
+
+echo "Done"
